22ea58f2f4b54f9e7b285725e9e131cafb155221
gravityfox <gravityreallyhatesme@gmail.com>
2015-10-21 22:43:24 -0700

    OOPS I SCREWED UP Forgot to stage files for deletion... oops...

--- tk/elektrofuchse/fox/foxguard/flags/util/ActiveFlags.java
+++
@@ -1,8 +0,0 @@
-package tk.elektrofuchse.fox.foxguard.flags.util;
-
-/**
- * Created by Fox on 8/17/2015.
- */
-public enum ActiveFlags {
-    BLOCK_PLACE, BLOCK_BREAK
-}
--- tk/elektrofuchse/fox/foxguard/flags/util/PassiveFlags.java
+++
@@ -1,7 +0,0 @@
-package tk.elektrofuchse.fox.foxguard.flags.util;
-
-/**
- * Created by Fox on 8/17/2015.
- */
-public enum PassiveFlags {
-}
