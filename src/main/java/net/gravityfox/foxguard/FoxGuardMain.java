47ffb5c124990f5ac53dc9efde79daaa4be6837e
gravityfox <gravityreallyhatesme@gmail.com>
2015-11-28 03:45:27 -0800

    Bump Version

--- src/main/java/net/gravityfox/foxguard/FoxGuardMain.java
+++ src/main/java/net/gravityfox/foxguard/FoxGuardMain.java
@@ -65,7 +65,7 @@ import static net.gravityfox.foxguard.util.Aliases.HANDLERS_ALIASES;
 @Plugin(id = "foxguard", name = "FoxGuard", version = FoxGuardMain.PLUGIN_VERSION)
 public class FoxGuardMain {
 
-    public static final String PLUGIN_VERSION = "0.9";
+    public static final String PLUGIN_VERSION = "0.9.1";
 
     private static FoxGuardMain instance;
 
