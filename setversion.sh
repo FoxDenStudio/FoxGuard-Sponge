fef4196369bf049816dce16df34d8e1e037d9c00
gravityfox <gravityreallyhatesme@gmail.com>
2015-12-06 14:24:40 -0800

    Travis test 3

--- setversion.sh
+++ setversion.sh
@@ -2,5 +2,8 @@
 
 if [ -n ${TRAVIS_TAG} ]
 then
+    echo "This is a tagged commit! (Tag: ${TRAVIS_TAG}) Injecting version into main class..."
     sed -i.bak "s/\"SNAPSHOT\"/\"${TRAVIS_TAG/#v/}\"/" ./src/main/java/net/gravityfox/foxguard/FoxGuardMain.java
+else
+    echo "This is not a tagged commit! Skipping injection..."
 fi
