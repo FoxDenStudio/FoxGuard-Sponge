c970da0ae53fec28b022e0c37008dd7526e6d7c9
Mike "Fox" Liu <gravityreallyhatesme@gmail.com>
2018-05-17 14:27:10 -0700

    Missed a spot.

--- src/main/java/net/foxdenstudio/sponge/foxguard/plugin/config/ListenerModule.java
+++ src/main/java/net/foxdenstudio/sponge/foxguard/plugin/config/ListenerModule.java
@@ -1,7 +1,6 @@
 package net.foxdenstudio.sponge.foxguard.plugin.config;
 
 import net.foxdenstudio.sponge.foxguard.plugin.FoxGuardMain;
-import net.foxdenstudio.sponge.foxguard.plugin.listener.PlayerMoveListener;
 import net.foxdenstudio.sponge.foxguard.plugin.listener.PlayerMoveListenerNew;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 
@@ -11,16 +10,16 @@ import javax.annotation.Nullable;
 public enum ListenerModule {
 
     MOVEMENT("movement", "true", "Sets movement on or off. \"hud\" enables listener, but only for /fg here functionality\n" +
-            " Options: true, false, hud"){
+            " Options: true, false, hud") {
         @Override
         public String setup(String setting) {
             FoxGuardMain plugin = FoxGuardMain.instance();
             boolean full;
-            if(setting.equalsIgnoreCase("false")){
+            if (setting.equalsIgnoreCase("false")) {
                 return setting;
-            } else if (setting.equalsIgnoreCase("true")){
+            } else if (setting.equalsIgnoreCase("true")) {
                 full = true;
-            } else if (setting.equalsIgnoreCase("hud")){
+            } else if (setting.equalsIgnoreCase("hud")) {
                 full = false;
             } else {
                 setting = getDefaultValue();
@@ -31,14 +30,13 @@ public enum ListenerModule {
             //plugin.registerListeners(pml.new Listeners());
             return setting;
         }
-    },
-    ;
+    },;
 
     private final String name;
     private final String defaultValue;
     private final String comment;
 
-    ListenerModule(@Nonnull String name, @Nonnull  String defaultValue, @Nullable String comment) {
+    ListenerModule(@Nonnull String name, @Nonnull String defaultValue, @Nullable String comment) {
         this.name = name;
         this.defaultValue = defaultValue;
         this.comment = comment != null && comment.isEmpty() ? null : comment;
@@ -48,7 +46,7 @@ public enum ListenerModule {
         return name;
     }
 
-    public String setup(String setting){
+    public String setup(String setting) {
         return setting;
     }
 
